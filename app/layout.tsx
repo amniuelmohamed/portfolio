3a1af862be4132e028cc897541b8d023296375ed
Amniuel Merabet Mohamed <amniuel.mohamed@gmail.com>
2023-12-31 23:17:37 +0100

    Fix the not working overflow-x hidden in Safari

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className="overflow-x-hidden">
             <body
-                className={`${inter.className} bg-gray-50 text-gray-950 relative overflow-x-hidden`}
+                className={`${inter.className} bg-gray-50 text-gray-950 relative`}
             >
                 <div
                     className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] 
